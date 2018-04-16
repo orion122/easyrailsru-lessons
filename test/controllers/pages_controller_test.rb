9d77da46e8acafbb593e665366fb23881e949571
ramazan <yaru@bk.ru>
2018-04-16 10:33:14 +0300

    second commit

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get landing" do
+    get pages_landing_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+end
